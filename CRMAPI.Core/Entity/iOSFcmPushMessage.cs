974153ea69b7169e9535a373fbfe36d8a7e1bd73
IT_游鵬勳 <cclinyu@gmail.com>
2017-12-06 10:29:51 +0800

    iOSFcmPushMessage 簽入

--- CRMAPI.Core/Entity/iOSFcmPushMessage.cs
+++ CRMAPI.Core/Entity/iOSFcmPushMessage.cs
@@ -5,24 +5,32 @@ using System.Web;
 
 namespace CRMAPI.Core.Entity
 {
-    public class FCMPushMessage
+    public class iOSFcmPushMessage
     {
         private string _regID;
         private string _APIKey;
         private string _Message;
 
+        /// <summary>
+        /// 訊息內容
+        /// </summary>
         public string Message
         {
             get { return _Message; }
             set { _Message = value; }
         }
-
+        /// <summary>
+        /// firebase的application驗證key
+        /// </summary>
         public string APIKey
         {
             get { return _APIKey; }
             set { _APIKey = value; }
         }
 
+        /// <summary>
+        /// 手機app上的token字串
+        /// </summary>
         public string RegID
         {
             get { return _regID; }
